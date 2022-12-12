67c1e5ae6a6ed67284f66c1e4be8c84086a7637f
Dragonstei <stanboer@gmail.com>
2022-12-12 17:13:25 +0100

    Refactored code and updated README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# GorillaGripp
+# GorillaGripp - Werkplaatsproject S1
 Custom script to work with the LEGO EV3 Mindstorms - Education set
 
 Documentation found on: https://pybricks.com/ev3-micropython/
@@ -6,4 +6,13 @@ Documentation found on: https://pybricks.com/ev3-micropython/
 ## Instructions
 
 When using Visual Studio Code in combination with the EV3 MicroPython extension, make sure
-to set the "End of Line Sequence" from CRLF to LF (because we are uploading to a Linux based system)
+to set the "End of Line Sequence" from CRLF to LF (Windows Line Ending to UNIX Line Ending)
+
+**Git currently handles EOL automatically, need to figure out a way to stop this (keeps changing LF back to CRLF)**
+
+## Project team
+
+Stan Boer         -   0931006 
+Rik van Daal      -   1047875
+Jeremy de Kroon   -   1015377 
+Andy Vos          -   0945183
--- src/main.py
+++ src/main.py
@@ -1,169 +1,40 @@
 #!/usr/bin/env pybricks-micropython
+# Make sure to put the line above as the first line of code
 
-"""
-Example LEGO® MINDSTORMS® EV3 Robot Arm Program
------------------------------------------------
+# GorillaGripp 2022 - Werkplaatsproject S1
+# Stan Boer
+# Rik van Daal
+# Jeremy de Kroon
+# Andy Vos
 
-This program requires LEGO® EV3 MicroPython v2.0.
-Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
+# [Stan] Ideas to improve software:
+# Make use of the cmd and sys library to create an environment in a terminal
+# where it's possible to read/send data to the robot while running
 
-Building instructions can be found at:
-https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#building-core
-"""
+# Imports from Standard Library (remove if not planning to create cmd environment)
+import cmd, sys
 
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
-from pybricks.parameters import Port, Stop, Direction, SoundFile
-from pybricks.tools import wait
+# User imports
+import robot
 
-# Initialize the EV3 Brick
-ev3 = EV3Brick()
+# [Stan] Explaination on writing a main function: https://realpython.com/python-main-function/
+def main():
+    # [Stan] Directions and postions should be stored more neatly
+    LEFT = 180
+    RIGHT = 0
+    DirectionSpeed = 40
 
-# Configure the gripper motor on Port A with default settings.
-gripper_motor = Motor(Port.D)
+    ev3 = robot.Robot()
 
-# Configure the elbow motor. It has an 8-teeth and a 40-teeth gear
-# connected to it. We would like positive speed values to make the
-# arm go upward. This corresponds to counterclockwise rotation
-# of the motor.
-elbow_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE, [8, 40])
+    ev3.initMotors()
+    ev3.setMotorControls()
 
-# Configure the motor that rotates the base. It has a 12-teeth and a
-# 36-teeth gear connected to it. We would like positive speed values
-# to make the arm go away from the Touch Sensor. This corresponds
-# to counterclockwise rotation of the motor.
-base_motor = Motor(Port.C, Direction.COUNTERCLOCKWISE, [12, 36])
+    ev3.initSensors()
 
-# Limit the elbow and base accelerations. This results in
-# very smooth motion. Like an industrial robot.
-elbow_motor.control.limits(speed = 100, acceleration = 120)
-base_motor.control.limits(speed = 100, acceleration = 120)
+    ev3.initRobot()
 
-# Set up the Touch Sensor. It acts as an end-switch in the base
-# of the robot arm. It defines the starting point of the base.
-base_switch = TouchSensor(Port.S1)
+    while True:
+        ev3.robotCycle(LEFT, RIGHT, DirectionSpeed)
 
-# Set up the Color Sensor. This sensor detects when the elbow
-# is in the starting position. This is when the sensor sees the
-# white beam up close.
-elbow_switch = TouchSensor(Port.S3)
-
-ei_sensor = ColorSensor(Port.S4)
-
-# Initialize the elbow. First make it go down for one second.
-# Then make it go upwards slowly (15 degrees per second) until
-# the Color Sensor detects the white beam. Then reset the motor
-# angle to make this the zero point. Finally, hold the motor
-# in place so it does not move.
-elbow_motor.run_time(-30, 1000)
-elbow_motor.run(15)
-while not elbow_switch.pressed():
-    wait(10)
-elbow_motor.reset_angle(0)
-elbow_motor.hold()
-
-# Initialize the base. First rotate it until the Touch Sensor
-# in the base is pressed. Reset the motor angle to make this
-# the zero point. Then hold the motor in place so it does not move.
-base_motor.run(-60)
-while not base_switch.pressed():
-    wait(10)
-base_motor.reset_angle(0)
-base_motor.hold()
-
-# Initialize the gripper. First rotate the motor until it stalls.
-# Stalling means that it cannot move any further. This position
-# corresponds to the closed position. Then rotate the motor
-# by 90 degrees such that the gripper is open.
-gripper_motor.run_until_stalled(200, then = Stop.COAST, duty_limit = 30)
-
-gripper_motor.reset_angle(0)
-
-def initialize_gripper():
-    while gripper_motor.angle() > -900:
-        gripper_motor.run(-200)
-    gripper_motor.hold()
-    base_motor.hold()
-
-initialize_gripper()
-
-# Define the three destinations for picking up and moving the wheel stacks.
-LEFT = 180
-MIDDLE = 90
-RIGHT = 0
-
-ResetPos = 90
-DirectionSpeed = 40
-
-def robot_beep(amount):
-    # Play x amount of beeps to indicate that a process is complete.
-    for i in range(amount):
-        ev3.speaker.beep()
-        wait(100)
-
-ev3.speaker.play_file(SoundFile.READY)
-ev3.speaker.say('GorillaGripper ready')
-
-def robot_grab():
-    # This function makes the robot base rotate to the indicated
-    # position. There it lowers the elbow, closes the gripper, and
-    # raises the elbow to pick up the object.
-    print("[2] Grabbing egg")
-    # Lower the arm.
-    elbow_motor.run_target(60, -42)
-    # Close the gripper to grab the wheel stack.
-    gripper_motor.run_until_stalled(200, then = Stop.HOLD, duty_limit = 60)
-    # Raise the arm to lift the wheel stack.
-    elbow_motor.run_target(60, 0)
-
-def robot_drop():
-    base_motor.run_target(60, RIGHT)
-    # This function makes the robot base rotate to the indicated
-    # position. There it lowers the elbow, opens the gripper to
-    # release the object. Then it raises its arm again.
-    print("[3] Dropping egg")
-    # Lower the arm to put the wheel stack on the ground.
-    elbow_motor.run_target(60, -45)
-    # Open the gripper to release the wheel stack.
-    gripper_motor.run_target(200, -900)
-    # Raise the arm.
-    elbow_motor.run_target(60, 0)
-
-def robot_reset_position():
-    print("[4] Resetting position")
-    base_motor.run_target(60, ResetPos)
-    if base_motor.angle() == ResetPos:
-        robot_cycle(LEFT, RIGHT, DirectionSpeed)
-
-def robot_cycle(position, position2, direction):
-    while base_motor.angle() != position:
-        try:
-            base_motor.run(direction)
-            if robot_detect():
-                break 
-        except:
-            print("[!] Error, couldn't move robot arm")
-            break
-    if base_motor.angle() == position:
-        base_motor.hold()
-        robot_cycle(position2, position, -direction)
-    print("End of robot cycle")
-
-def robot_detect():
-    #[Stan] rgb(0, 0, 0) is too sensitive, (2, 2, 2) works in a light environment. reflection() adds a red light to the camera, can cause confusion with RGB detection
-    # Ambient can be used to set the detection values for seperate environments. Ex. a dark ambient room where ambient() < 10 can use different detection values than ambient() > 10
-    if ei_sensor.color():
-        print("[1] Egg detected")
-        robot_beep(1)
-        base_motor.hold()
-        robot_grab()
-        robot_drop()
-        robot_reset_position()
-        return True
-    else:
-        return False
-
-try:
-    robot_cycle(LEFT, RIGHT, DirectionSpeed)
-except:
-    print("[!] Can't start cycle")
+if __name__ == "__main__":
+    main()
